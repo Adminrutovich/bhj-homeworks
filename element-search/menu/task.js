36d75d41eb39f6caf72b50d02925e02662bd6803
Admin Rutovich <380662620485@ukr.net>
2023-03-22 16:31:43 +0200

    first commit menu

---
+++ element-search/menu/task.js
@@ -0,0 +1,13 @@
+
+let menuLinks = Array.from(document.querySelectorAll(".menu__link"));
+
+for (let i = 0; i < menuLinks.length; i++) {
+  let menuItem = menuLinks[i].closest(".menu__item");
+  let menuSub = menuItem.querySelector(".menu_sub");
+  if (menuSub) {
+    menuLinks[i].onclick = function () {
+      menuSub.classList.toggle("menu_active");
+      return false;
+    }
+  }
+};
